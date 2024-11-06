a58f78ca8a470e24f17446ef30dd9fafa0b7b938
5b82e901-944e-4181-9ef4-126ab1006aec <56427509+5b82e901@users.noreply.github.com>
2024-11-06 23:12:32 +0300

    Delete TCPOpt.cmd

--- TCPOpt.cmd
+++
@@ -1,8 +0,0 @@
-netsh int tcp set global autotuninglevel=disabled > nul
-netsh int tcp set global chimney=disabled > nul
-netsh int tcp set global rss=enabled > nul
-netsh int tcp set global rsc=disabled > nul
-netsh int tcp set global ecncapability=enabled > nul
-netsh int tcp set global timestamps=disabled > nul
-netsh int tcp set global initialRto=2000 > nul
-powershell -Command "Set-NetTCPSetting -SettingName InternetCustom -MinRto 300" > nul
